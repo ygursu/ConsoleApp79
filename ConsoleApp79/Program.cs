d1f53c06c6561c3696366cbb59c576a4b936ec8d
ygursu <yasemingrs@gmail.com>
2024-02-21 02:59:33 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp79/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedenikisayinintoplamininkupunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen 1. sayıyı giriniz");
+            int sayi1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. sayıyı giriniz");
+            int sayi2 = Int32.Parse(Console.ReadLine());
+
+            int toplam = sayi1 + sayi2;
+            int karesi = toplam * toplam * toplam;
+
+            Console.WriteLine("Klavyeden girilen sayının toplamının küpünü={0}", karesi);
+            Console.ReadLine();
+
+        }
+    }
+}
